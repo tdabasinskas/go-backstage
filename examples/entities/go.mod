fedc98ebdb28b04a217bbb44384e3c5904737281
Tomas Dabasinskas <tomas@dabasinskas.net>
2023-01-23 19:21:32 +0200

    Add initial example

---
+++ examples/entities/go.mod
@@ -0,0 +1,7 @@
+module exampleentities
+
+go 1.19
+
+replace github.com/tdabasinskas/go-backstage => ../..
+
+require github.com/tdabasinskas/go-backstage v0.0.0-20230117064629-2a4ac9398d92
---
+++ examples/entities/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/tdabasinskas/go-backstage/backstage"
+)
+
+func main() {
+	baseURL, ok := os.LookupEnv("BACKSTAGE_BASE_URL")
+	if !ok {
+		baseURL = "http://localhost:7007/api/"
+	}
+
+	log.Println("Initializing Backstage client...")
+	c, _ := backstage.NewClient(baseURL, "default", nil)
+
+	log.Println("Getting all entities...")
+	if entities, _, err := c.Catalog.Entities.List(context.Background(), nil); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Entities: %v", entities)
+	}
+
+	log.Println("Getting component entities...")
+	if entities, _, err := c.Catalog.Entities.List(context.Background(), &backstage.ListEntityOptions{
+		Filters: backstage.ListEntityFilter{
+			"kind": "component",
+		},
+	}); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Component entities: %v", entities)
+	}
+
+	log.Println("Getting specific component entity by UID...")
+	if entity, _, err := c.Catalog.Entities.Get(context.Background(), "06f15cc8-b6b4-4e44-a9bd-1579029f8fb7"); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Component entity: %v", entity)
+	}
+
+	log.Println("Getting specific component by name...")
+	if component, _, err := c.Catalog.Components.Get(context.Background(), "backstage", ""); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Component: %v", component)
+	}
+}
