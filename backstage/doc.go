bc30e7b6f130cdc75d2d99671aa59ece3053ede1
Tomas Dabasinskas <tomas@dabasinskas.net>
2023-01-24 08:38:55 +0200

    Add GoDoc for the package

---
+++ backstage/doc.go
@@ -0,0 +1,29 @@
+/*
+Package backstage provides a client for the Backstage API.
+
+# Usage
+
+Add the package to your project as following:
+
+	import "github.com/tdabasinskas/go-backstage"
+
+Once imported, create a new Backstage API client to access different parts of Backstage API:
+
+	client, err := backstage.NewClient(baseURL, "default", nil)
+
+If you want to use a custom HTTP client (for example, to handle authentication, retries or different timeouts), you can pass it as the
+third argument:
+
+	httpClient := &http.Client{}
+	client, err := backstage.NewClient(baseURL, "default", httpClient)
+
+The client than can be used to access different parts of the API, e.g. get the list of entities, sorted in specific order:
+
+	entities, response, err := c.Catalog.Entities.s.List(context.Background(), &ListEntityOptions{
+			Filters: ListEntityFilter{},
+			Fields:  []string{},
+			Order:   []ListEntityOrder{{ Direction: OrderDescending, Field: "metadata.name" },
+		},
+	})
+*/
+package backstage
